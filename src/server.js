fb4e22a65a6a49103b1a1fcb169a9bcad634c0e2
AllefRamos14 <mariaalice6123@gmail.com>
2026-05-04 09:29:36 -0300

    fix: Atualizaçao do nome do dominio

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://barbeiro-front-end.vercel.app",
+  "https://barbearia-washington.vercel.app",
 ];
 
 app.use(
